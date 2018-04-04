6f5573f6d8a694ce94b41b0ccfbf6f6307ddb648
leeanneNYC <35641934+leeanneNYC@users.noreply.github.com>
2018-04-04 12:19:44 -0400

    Add files via upload

--- clock/script.js
+++ clock/script.js
@@ -1,23 +1,37 @@
 $(document).ready(function() {
 
   setTimeout(function(){
-  	 $(".wake-up-text") .addClass("alarming");
+  	 $(".wake-up-text") .addClass("show");
   	 $(".w4") .addClass("alarming");
+  	 $(".carroll") .addClass("alarming");
   	 $(".min") .addClass("alarming");
+  	 $(".min2") .addClass("alarming");
   	 $(".button") .addClass("alarming");
   	 $(".e") .addClass("alarming");
+  	 $(".f") .addClass("alarming");
+  	 $(".hour") .addClass("remove");
+  	 $(".minute") .addClass("remove");
+  	 $(".second") .addClass("remove");
+  	 $(".train") .addClass("remove");
   	 //$("body").addClass("alarming");
 
   }, 3000);
 
 
-  	$(".button").click(function(){
-  	$(".wake-up-text").removeClass("alarming");
+  	$(".button").click(function(){;
+  	$(".wake-up-text").removeClass("show");
   	$("body").removeClass("alarming");
   	$(".w4").removeClass("alarming");
+  	$(".carroll") .removeClass("alarming");
   	$(".min").removeClass("alarming");
+  	$(".min2") .removeClass("alarming");
   	$(".button").removeClass("alarming");
   	$(".e").removeClass("alarming");
+  	$(".f").removeClass("alarming");
+  	$(".hour") .removeClass("remove");
+  	$(".minute") .removeClass("remove");
+  	$(".second") .removeClass("remove");
+  	$(".train") .removeClass("remove");
 
   });
 
